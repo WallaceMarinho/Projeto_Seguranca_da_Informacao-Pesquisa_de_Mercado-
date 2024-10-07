951524a34951c9f94488f5a5e93e4f64ef481484
Rafael-Caje <rafa_pc_sjc@hotmail.com>
2024-10-07 19:12:25 -0300

    fix: DDL, ajuste para criar tabelas

--- so_main.py
+++ so_main.py
@@ -1,14 +1,15 @@
 from so_create_adm import create_default_admin
 from so_terms_login import main
-from so_create_db import connect  # Adicione isso para importar a função de conexão
+from so_create_db import connect, create_tables 
 
 if __name__ == "__main__":
     tunnel, mydb = connect()
 
-    if mydb:  # Verifica se a conexão foi bem-sucedida
+    if mydb:
         try:
+            create_tables(mydb)					# Cria as Tabelas
             create_default_admin(mydb)  # Passa a conexão para a função
-            main(tunnel, mydb)  # Passa a conexão para o main também
+            main(tunnel, mydb)					# Passa a conexão para o main também
         finally:
             mydb.close()
             tunnel.close()
